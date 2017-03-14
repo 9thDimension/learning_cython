2d28996e66d9030ba293213c191255f47573882f
Xavier <xavier.dcruz0@gmail.com>
2017-03-14 12:31:37 +0000

    doesn't work

--- helloer.pyx
+++ helloer.pyx
@@ -1,13 +1,13 @@
 
 cdef extern from "HelloWorlder.h" namespace "std":
     cdef cppclass HelloWorlder:
-        HelloWorlder(string) except +
+        HelloWorlder(str) except +
         str name
         void sayHello()
 
 cdef class PyHelloWorlder:
     cdef HelloWorlder *thisptr
-    def __cinit__(self, string theName):
+    def __cinit__(self, str theName):
         self.thisptr = new HelloWorlder(theName)
     def __dealloc__(self):
         del self.thisptr
