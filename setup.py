2b7cf44c6fa82efabafbf495ab0722084beca4ee
Xavier <xavier.dcruz0@gmail.com>
2017-03-14 12:34:11 +0000

    still doesn't work

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize(
+setup(ext_modules = cythonize(Extension(
 
     "helloer.pyx",
     sources=["helloer.pyx" ,"HelloWorlder.cpp"],
     language="c++",
 
-))
+)))
